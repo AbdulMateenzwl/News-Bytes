71b734b951bc500029359f87002cbdbad8bf9279
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-07-16 15:42:09 +0500

    news using api

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div >
         <Navbar/>
-        <News/>
+        <News pageSize={5}/>
       </div>
     )
   }
--- src/componants/NewsItem.jsx
+++ src/componants/NewsItem.jsx
@@ -8,16 +8,16 @@ export class NewsItem extends Component {
 
     render() {
 
-        let { title, description, imageUrl ,newsUrl} = this.props;
+        let { title, description, imageUrl ,newsUrl,content} = this.props;
 
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                 <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl?"https://demofree.sirv.com/nope-not-here.jpg":imageUrl} className="card-img-top" alt={content} />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description} . . .</p>
-                        <a href={newsUrl} className="btn btn-sm btn-primary">Read more</a>
+                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read more</a>
                     </div>
                 </div>
             </div>
