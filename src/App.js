af03389c980d097dd2120aa01d8346dda02b8a08
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-07-18 17:24:41 +0500

    router settings updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './componants/Navbar';
 import News from './componants/News';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Navbar />
+        <Router>
+          <Navbar />
           <Routes>
-              <Route exact path="/" element={<News key="general" pageSize={9} country="in" category="general" />} />
-              <Route exact path="/business" element={<News key="business" pageSize={9} country="in" category="business" />} />
+            <Route path="/" element={<News key="general" pageSize={9} country="in" category="general" />} />
+            <Route exact path="/business" element={<News key="business" pageSize={9} country="in" category="business" />} />
           </Routes>
+        </Router>
         {/* <News pageSize={9} country="in" category="science" /> */}
       </div>
     )
